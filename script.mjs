dd57d6c8be9b10bee018194bcae92fb1f0d6fa74
Aamir Nakhwa <aamirnakhwa@yahoo.com>
2023-06-23 09:58:46 +0500

    Converted to es6

--- script.mjs
+++ script.mjs
@@ -1,25 +1,26 @@
-function calculate() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultElement = document.getElementById("result");
-    var errorElement = document.getElementById("error");
+const calculate = () => {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const resultElement = document.getElementById("result");
+  const errorElement = document.getElementById("error");
 
-    if (isNaN(num1) || isNaN(num2)) {
-      errorElement.textContent = "Please enter valid numbers";
-      resultElement.textContent = "";
-    } else if (num2 === 0) {
-      errorElement.textContent = "Cannot divide by zero";
-      resultElement.textContent = "";
-    } else {
-      errorElement.textContent = "";
-      var sum = num1 + num2;
-      var difference = num1 - num2;
-      var product = num1 * num2;
-      var quotient = num1 / num2;
-      resultElement.innerHTML =
-        "Sum: " + sum + "<br>" +
-        "Difference: " + difference + "<br>" +
-        "Product: " + product + "<br>" +
-        "Quotient: " + quotient;
-    }
+  if (isNaN(num1) || isNaN(num2)) {
+    errorElement.textContent = "Please enter valid numbers";
+    resultElement.textContent = "";
+  } else if (num2 === 0) {
+    errorElement.textContent = "Cannot divide by zero";
+    resultElement.textContent = "";
+  } else {
+    errorElement.textContent = "";
+    const sum = num1 + num2;
+    const difference = num1 - num2;
+    const product = num1 * num2;
+    const quotient = num1 / num2;
+    resultElement.innerHTML =
+      `Sum: ${sum}<br>` +
+      `Difference: ${difference}<br>` +
+      `Product: ${product}<br>` +
+      `Quotient: ${quotient}`;
   }
+};
+document.getElementById("calculateButton").addEventListener("click", calculate);
